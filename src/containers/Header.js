2eab6892ccd0c2c0fa52bc0059c4c53a4f77bacd
Tim Roper <tim.roper@interfisystems.com>
2016-11-04 09:59:52 +1100

    Routes enabled

---
+++ src/containers/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+
+class Header extends React.Component {
+	render() {
+		return (
+			<nav className="navbar navbar-default">
+				<div className="container-fluid">
+					<div className="navbar-header">
+						<Link className="navbar-brand" to="/">React2Gifs</Link>
+					</div>
+					<ul className="nav navbar-nav navbar-right">
+						<li className="nav-item">
+							<Link className="nav-link" to="/Login">Login</Link>
+						</li>
+						<li className="nav-item">
+							<Link className="nav-link" to="/Signup">Signup</Link>
+						</li>
+
+					</ul>
+				</div>
+			</nav>
+		);
+	}
+}
+
+function mapStateToProps(state) {
+	return {}
+}
+
+export default connect(mapStateToProps)(Header)
